b88cf756fc3d669b6640e8baee24f258601bd976
Matheus Schaffer <teuzin@gmail.com>
2024-01-30 23:26:21 -0300

    Add section skills and finalizated presentation

--- src/componentes/sections/Presentation.js
+++ src/componentes/sections/Presentation.js
@@ -1,8 +1,19 @@
+import styles from './Presentation.module.css'
+import ButtonA from '../elements/ButtonA'
+
 function Presentation() {
     return (
-        <div id="Presentation">
-            <h1>Aqui ficará meu presentation</h1>
-            <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+        <div className={styles.presentation} id="Presentation">
+            <h4><strong>Bem-vindo ao meu Portfólio!</strong></h4>
+            <h1>Olá, eu sou Matheus Schaffer!</h1>
+            <p>
+                Sou um resolvedor de problemas nato, apaixonado por soluções práticas e inovadoras.<br />
+                Como analista de sistemas, busco resolver as barreiras que impedem os clientes e<br />
+                usuários de terem sucesso na utilização de nossos produtos. Fidelizar e criar novas<br />
+                oportunidades de negócio são os meus motivadores diários, estes refletem em receitas<br />
+                diretas e indiretas para os negócios.
+            </p>
+            <ButtonA link='https://github.com/matheusmschaffer' text='Conecte-se comigo!' />
         </div >
     )
 }
--- src/componentes/sections/Skills.js
+++ src/componentes/sections/Skills.js
@@ -1,10 +1,22 @@
+import style from './Skills.module.css'
+import javascript from '../../Images/skills/javascript.svg'
+import html from '../../Images/skills/html.svg'
+import css from '../../Images/skills/css.svg'
+import react from '../../Images/skills/react.svg'
+import typescript from '../../Images/skills/typescript.svg'
+
 function Skills() {
     return (
-        <div id="Skills">
-            <h1>
-                Aqui ficará minhas habilidades!
-                <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-            </h1>
+        <div id="Skills" className={style.skill}>
+            <h1>Habilidades</h1>
+            <p>Conheça um pouco das minhas principais habilidades e conhecimentos.</p>
+            <div>
+                <img src={javascript}></img>
+                <img src={html}></img>
+                <img src={css}></img>
+                <img src={react}></img>
+                <img src={typescript}></img>
+            </div>
         </div>
     )
 }
