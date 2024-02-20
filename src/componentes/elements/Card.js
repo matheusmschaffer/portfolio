2dd7497eba0a599acc696936aee096abc8192f51
Matheus Schaffer <teuzin@gmail.com>
2024-02-19 22:06:01 -0300

    Effects

--- src/componentes/elements/Card.js
+++ src/componentes/elements/Card.js
@@ -1,19 +1,37 @@
 import styles from './Card.module.css'
 import ButtonB from './ButtonB'
+import { useState } from 'react'
 
 function Card({ img, title, tech, description, repo, site }) {
+
+    const [info, setInfo] = useState(false)
+
+    function InfoOn() {
+        setInfo(true)
+    }
+
+    function InfoOff() {
+        setInfo(false)
+    }
+
     return (
-        <div className={styles.card}>
-            <a href={site} target="_blank">
+        <div onMouseLeave={InfoOff} className={styles.card}>
+            <a onMouseEnter={InfoOn} href={site} target="_blank">
                 <img src={img} alt="Error"></img>
             </a>
-            <div className={styles.div2}>
-                <h3>{title}</h3>
-                <p><strong>Tecnologia: </strong>{tech}</p>
-                <p>{description}</p>
-                <ButtonB text='Acesse o repositório' link={repo} />
-            </div>
+
+            {
+                info === true && (
+                    <div className={styles.div2}>
+                        <h3>{title}</h3>
+                        <p><strong>Tecnologia: </strong>{tech}</p>
+                        <p>{description}</p>
+                        <ButtonB text='Acesse o repositório' link={repo} />
+                    </div>
+                )
+            }
         </div>
+
     )
 }
 
--- src/componentes/sections/Presentation.js
+++ src/componentes/sections/Presentation.js
@@ -1,11 +1,47 @@
 import styles from './Presentation.module.css'
 import ButtonA from '../elements/ButtonA'
+import { useEffect, useState } from 'react'
 
 function Presentation() {
+    const [text, setText] = useState('');
+    const toRotate = ['Matheus Schaffer!!!', 'Desenvolvedor full stack!!!', 'Product Manager!!!'];
+    const [loop, setLoop] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const min = 150;
+    const max = 300
+    const [delta, setDelta] = useState(Math.floor(Math.random() * max - min + 1) + min);
+
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            toType()
+        }, delta)
+        return () => { clearInterval(ticker) }
+    }, [text])
+
+    const toType = () => {
+        let i = loop % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+        setText(updatedText);
+
+        if (!isDeleting) { setDelta(Math.floor(Math.random() * max - min + 1) + min) }
+
+        if (!isDeleting && updatedText === fullText) {
+            setIsDeleting(true);
+            setDelta(100);
+        } else if (isDeleting && updatedText === '') {
+            setIsDeleting(false);
+            setDelta(Math.floor(Math.random() * max - min + 1) + min);
+            setLoop(loop + 1);
+        }
+
+    }
+
     return (
         <div className={styles.presentation} id="Presentation">
             <h4><strong>Bem-vindo ao meu Portfólio!</strong></h4>
-            <h1>Olá, eu sou Matheus Schaffer!</h1>
+            <h1>Olá, eu sou {text}</h1>
             <p>
                 Sou um resolvedor de problemas nato, apaixonado por soluções práticas e inovadoras.<br />
                 Como analista de sistemas, busco resolver as barreiras que impedem os clientes e<br />
